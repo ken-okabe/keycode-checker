1f112fc7fcb97cc4cd58aacbd9cc6226e6e21595
stken2050 <stken2050@gmail.com>
2022-10-09 19:44:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# keycode-checker
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-1nlwk2)
---
+++ index.js
@@ -0,0 +1,21 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+
+window.document.onkeydown = function (evt) {
+  document.getElementById('debug').value =
+    `{` +
+    '\n' +
+    `  shiftKey: ${evt.shiftKey},` +
+    '\n' +
+    `  ctrlKey: ${evt.ctrlKey},` +
+    '\n' +
+    `  altKey: ${evt.altKey},` +
+    '\n' +
+    `  metakey: ${evt.metaKey},` +
+    '\n' +
+    `  code: ${evt.code}` +
+    '\n' +
+    `}`;
+};
